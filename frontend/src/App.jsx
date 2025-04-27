d0d72d5a2bfdc274b8abf9e21c71ae455aa7d291
Thrinath008 <148782730+Thrinath008@users.noreply.github.com>
2025-04-27 10:15:24 +0530

    added frontend to this project
    just the basic structure

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+
+
+function App(){
+   return (
+    <div className="App">
+      <h1>this is the student recomendation page</h1>
+    </div>
+   )
+}
+
+export default App;
